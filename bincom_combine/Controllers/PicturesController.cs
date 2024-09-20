25e24e66c39802cdbde954c13f02374a6583eb96
Ifeoluwa Janet Ibukun-ojo <iibukunojo@gmail.com>
2024-09-20 17:52:58 +0100

    picture update

--- bincom_combine/Controllers/PicturesController.cs
+++ bincom_combine/Controllers/PicturesController.cs
@@ -16,15 +16,26 @@ namespace bincom_conbine.Controllers
             _context = context;
         }
 
+        /// <summary>
+        /// Retrieves all pictures stored in the database.
+        /// </summary>
+        /// <returns>A list of pictures.</returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Picture>>> GetPictures()
         {
+            // Retrieve all pictures from the database
             return await _context.Pictures.ToListAsync();
         }
 
+        /// <summary>
+        /// Retrieves a specific picture by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the picture.</param>
+        /// <returns>The requested picture or 404 if not found.</returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<Picture>> GetPicture(int id)
         {
+            // Find the picture by its ID
             var picture = await _context.Pictures.FindAsync(id);
 
             if (picture == null)
@@ -34,16 +45,28 @@ namespace bincom_conbine.Controllers
 
             return picture;
         }
+
+        /// <summary>
+        /// Uploads a new picture and stores it in the database.
+        /// </summary>
+        /// <param name="file">The image file to be uploaded.</param>
+        /// <param name="name">The name of the picture.</param>
+        /// <param name="description">The description of the picture.</param>
+        /// <returns>The newly created picture details.</returns>
         [HttpPost]
         public async Task<ActionResult<Picture>> UploadPicture([FromForm] IFormFile file, [FromForm] string name, [FromForm] string description)
         {
+            // Check if a file has been uploaded
             if (file == null || file.Length == 0)
             {
                 return BadRequest("No file uploaded.");
             }
 
+            // Convert the uploaded file to byte array to store in the database
             using var memoryStream = new MemoryStream();
             await file.CopyToAsync(memoryStream);
+
+            // Create a new picture entity
             var picture = new Picture
             {
                 Name = name,
@@ -51,15 +74,23 @@ namespace bincom_conbine.Controllers
                 Image = memoryStream.ToArray()
             };
 
+            // Add the new picture to the database
             _context.Pictures.Add(picture);
             await _context.SaveChangesAsync();
 
+            // Return the created picture with a 201 Created status
             return CreatedAtAction(nameof(GetPicture), new { id = picture.Id }, picture);
         }
 
+        /// <summary>
+        /// Deletes a specific picture by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the picture to delete.</param>
+        /// <returns>No content on successful deletion, or 404 if not found.</returns>
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeletePicture(int id)
         {
+            // Find the picture by its ID
             var picture = await _context.Pictures.FindAsync(id);
             if (picture == null)
             {
