3fe8164c531a9347094b68019e9bdbe36004faa3
Ifeoluwa Janet Ibukun-ojo <iibukunojo@gmail.com>
2024-09-21 02:24:08 +0100

    dip

--- bincom_combine/Controllers/ContactUsController.cs
+++ bincom_combine/Controllers/ContactUsController.cs
@@ -3,6 +3,7 @@ using bincom_conbine.Data;
 using bincom_conbine.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace bincom_conbine.Controllers
 {
@@ -20,13 +21,21 @@ namespace bincom_conbine.Controllers
         /// <summary>
         /// Submits a contact form entry to the database.
         /// </summary>
-        /// <param name="contactUs">The contact details from the user.</param>
+        /// <param name="contactUsDto">The contact details from the user.</param>
         /// <returns>A success or failure message.</returns>
         [HttpPost]
-        public async Task<IActionResult> SubmitContact([FromBody] ContactUs contactUs)
+        public async Task<IActionResult> SubmitContact([FromBody] CreateContactUsDto contactUsDto)
         {
             if (ModelState.IsValid)
             {
+                var contactUs = new ContactUs
+                {
+                    Name = contactUsDto.Name,
+                    Email = contactUsDto.Email,
+                    Message = contactUsDto.Message,
+                    SubmittedAt = DateTime.UtcNow // Auto-set SubmittedAt
+                };
+
                 _context.ContactUsEntries.Add(contactUs);
                 await _context.SaveChangesAsync();
                 return Ok(new { message = "Contact form submitted successfully." });
@@ -44,5 +53,23 @@ namespace bincom_conbine.Controllers
             var submissions = await _context.ContactUsEntries.ToListAsync();
             return Ok(submissions);
         }
+    }
+
+    /// <summary>
+    /// Data Transfer Object (DTO) for submitting a contact form.
+    /// </summary>
+    public class CreateContactUsDto
+    {
+        [Required]
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Email { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string? Message { get; set; }
     }
 }
